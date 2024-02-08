bd7f596b9cba40f064796240b99dff2883f1cdfc
Ashe <ashemarsh42@gmail.com>
2024-02-08 16:48:17 +0000

    add key to topic card

--- src/Components/TopicCard.jsx
+++ src/Components/TopicCard.jsx
@@ -3,7 +3,7 @@ import {Card, CardActionArea, CardContent, Typography} from '@mui/material'
 export default function TopicCard({topic}){
     return(
         <>
-        <Card key={topic.slug}>
+        <Card >
             <CardActionArea href={`/articles?topic=${topic.slug}`}>
                 <CardContent>
                     <Typography gutterBottom variant='h5' component="div">{topic.slug}</Typography>
--- src/Components/TopicsPage.jsx
+++ src/Components/TopicsPage.jsx
@@ -5,7 +5,7 @@ export default function TopicsPage({topics}) {
         <section>
             <h2>Topics</h2>
                 {topics.map((topic) => {
-                    return <TopicCard topic={topic}/>
+                    return <TopicCard key={topic.slug} topic={topic}/>
                 })}
         </section>
     )
